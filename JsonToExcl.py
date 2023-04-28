61cff2faee82b991f946bb1c172c81e4269318e8
faltterers <71960204+faltterers@users.noreply.github.com>
2023-04-28 09:07:39 +0800

    Add files via upload

---
+++ JsonToExcl.py
@@ -0,0 +1,76 @@
+import os
+import jsonpath
+import json
+import numpy as np
+import pandas as pd
+import openpyxl
+# import xlwt
+
+# 根据键名取值
+def get_json_value(json_data,key_name):
+
+    key_value = jsonpath.jsonpath(json_data, '$..{key_name}'.format(key_name=key_name))
+    #key的值不为空字符串或者为empty（用例中空固定写为empty）返回对应值，否则返回empty
+   
+    return key_value
+
+# 以名字查找文件夹内对应的文件，并拷贝到另一个文件 path1为查找的文件夹路径，path2为拷贝到的文件夹路径，name1为查找的名称
+def main1(path1,path2,name1):
+    originalname = name1
+    replacename = name1
+    files = os.listdir(path1)  # 得到文件夹下的所有文件名称
+    for file in files: #遍历文件夹
+        if os.path.isdir(path1 + '\\' + file):
+            i=0
+            main1(path1 + '\\' + file)
+        else:
+            files2 = os.listdir(path1 + '\\')
+            i=1
+            for file1 in files2:
+                if originalname in file1:
+                    #用‘’替换掉 X变量 
+                    n1 = str(path1 + '\\' + str(file1))
+                    n2 = str(path2 + '\\' + file1.replace(str(file1),replacename+str(i)+'.jpg'))
+                    i=i+1
+                    # print(n1,n2,i)
+                    os.rename(n1,n2)
+
+# 以键名获得数组
+json_text=r'.\json\放入你自己的json路径'
+with open(json_text,encoding='utf8') as f:
+    content = json.load(f)
+
+filePath1 = '.\img\图片文件夹'
+filePath2 = '.\img_change\图片文件夹'
+names=get_json_value(content, "名称")
+
+# 循环检查
+for name in names:
+    # print(item)
+    main1(filePath1,filePath2,name)
+
+# 获取拷贝后的文件夹内的文件
+img=[]
+for i,j,k in os.walk(filePath2):
+    img=k
+
+# 将原名称的数组进行重构以便于后续数据插入
+arry_num = [[i] for i in names]
+j=0
+for item1 in names:
+    for item2 in img:
+        if item1 in item2:
+            arry_num[j].append((item2))
+    j=j+1
+
+
+#将数据导出到excl
+data = pd.DataFrame(arry_num)
+
+writer = pd.ExcelWriter('.\excl\\num.xlsx')		# 写入Excel文件
+data.to_excel(writer, 'page_1', float_format='%.5f')		# ‘page_1’是写入excel的sheet名
+writer.save()
+
+writer.close()
+
+
